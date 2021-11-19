26028bb59c74e42194c1c6d8c33abbc2ce91e996
Walter Andrade <walteraandrade@gmail.com>
2021-11-19 18:37:55 -0300

    stripped the app down to only to whats necessary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import '@fullcalendar/common/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
 
-
-import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
-
+import interactionPlugin from '@fullcalendar/interaction'
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <FullCalendar
-      plugins={[timeGridPlugin]} />
-    </div>
+    <FullCalendar
+    plugins={[interactionPlugin, timeGridPlugin]}
+    initialView='timeGridWeek'
+    nowIndicator={true}
+    editable={true}
+    initialEvents={[
+      { title: 'nice event', start: new Date() }
+    ]}
+  />
   )
 }
 
